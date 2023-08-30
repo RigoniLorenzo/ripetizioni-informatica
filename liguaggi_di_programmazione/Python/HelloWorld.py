48ebdc208206fb5f8ae45724bbf5676330204b49
Lorenzo Rigoni <rigoni.lorenzo.21@gmail.com>
2023-08-30 21:37:47 +0200

    Created Python examples

---
+++ liguaggi_di_programmazione/Python/HelloWorld.py
@@ -0,0 +1,6 @@
+#Come puoi vedere, in Python non c'è bisogno dei ; per delimitare le istrunzioni e neanche di una funzione main
+
+#Inoltre, non serve dichiarare le variabili, basta solo assegnarle
+msg = "Hello World!"
+
+print(msg)
---
+++ liguaggi_di_programmazione/Python/SumFunction.py
@@ -0,0 +1,9 @@
+#Per le funzioni, non c'è bisogno di dichiarare il tipo ritornato e neanche il tipo dei parametri
+#Basta usare la parola chiave def e usare l'indentazione
+
+def sumNumbers(a, b):
+    return a + b
+
+print(sumNumbers(4, 5))
+print(sumNumbers(5.8, 7.4))
+print(sumNumbers(12, 69.850))
