a1c83f43ac3fd63e07b9e1ea5e54194cbc1b9746
Lorenzo Rigoni <rigoni.lorenzo.21@gmail.com>
2023-09-01 11:15:07 +0200

    Created C exercise (Calculator)

---
+++ liguaggi_di_programmazione/Esercizi_in_C/Calculator.c
@@ -0,0 +1,30 @@
+/*ES.1 - CALCOLATRICE*/
+/*In questo esercizio simuleremo le 4 operazioni di una calcolatrice: 
+-somma
+-differenza
+-moltiplicazione
+-divisione
+Per ogni operazione dovrai creare una funzione e poi stampare i risultati con le variabili che ho già inserito (num1 e num2).
+Se hai dei dubbi scrivimi e buon lavoro!*/
+#include <stdio.h>
+
+/*SPAZIO PER LE FUNZIONI*/
+/*Scrivi qui sotto le 4 funzioni che ti sevono.
+ATTENZIONE al tipo restituito per la divisione*/
+
+/*FINE SPAZIO*/
+
+int main() {
+    /*Dichiarazione delle due variabili*/
+    int num1;
+    int num2;
+
+    /*Assegnazione delle due variabili*/
+    num1 = 12;
+    num2 = 5;
+
+    /*SPAZIO PER LE STAMPE*/
+    /*Scrivi qui sotto le 4 stampe dei risultati utilizzando le funzioni che hai creato sopra*/
+
+    /*FINE SPAZIO*/
+}
