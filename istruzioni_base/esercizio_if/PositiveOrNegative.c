20a41d0c9b823a76ec937862847c3d59a18dc8cd
Lorenzo Rigoni <rigoni.lorenzo.21@gmail.com>
2023-09-14 14:51:55 +0200

    Created exercise for if structure

---
+++ istruzioni_base/esercizio_if/PositiveOrNegative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/*In questo esercizio dovrai creare un programma che accetta un numero in input e mi dice se è positivo o negativo*/
+/*In più, se è positivo, mi deve anche dire se è pari o dispari*/
+
+int main() {
+    int inputNumber; /*Variabile in cui salvare il valore dato in input*/
+
+    /*Per prendere un numero in input si usa la funzione scanf con la sintassi:
+    scanf("%tipoVariabili", &nome della variabile in cui si salverà il valore);
+    es. scanf("%f", &floatNumber);
+    Per tipoVariabili si intendono quelle che abbiamo visto con la printf (%d, %f...)*/
+
+    printf("Inserisci un numero intero:\n");
+    
+    /*Inserisci qui la funzione per prendere in input il numero*/
+
+    /*Fine parte*/
+
+    /*Inserisci qui la parte di controllo e stampa*/
+
+    /*Fine parte*/
+
+    return 0;
+}
