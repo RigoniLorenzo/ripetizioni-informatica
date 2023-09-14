7a0ee327836779f835bb9a62b954e3e449099e1c
Lorenzo Rigoni <rigoni.lorenzo.21@gmail.com>
2023-09-14 15:09:52 +0200

    Created exercise for loops in C

---
+++ istruzioni_base/esercizio_loop/Calculator_2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/*In questo esercizio dovrai creare un programma che prenda in input una base e un esponente e calcoli la potenza*/
+/*Usa un ciclo a tua scelta, l'importante è che non usi le librerie con la potenza già implmentata.*/
+
+/*Funzione per calcolare la potenza*/
+int calcPower(int base, int exp) {
+    int res; /*Variabile per salvare il risultato (attenzione al valore inziale che gli dai)*/
+
+    /*Parte per il ciclo*/
+
+    /*Fine parte*/
+
+    return res;
+}
+
+int main() {
+    int base; /*Variabile per salvare la base*/
+    int exp; /*Variabile per salvare l'esponente*/
+
+    printf("Inserisci la base:\n");
+    /*Mettere la funzione per prendere in input la base*/
+
+    /*Fine parte*/
+
+    printf("Inserisci l'esponente:\n");
+    /*Mettere la funzione per prendere in input l'esponente*/
+    
+    /*Fine parte*/
+
+    printf("Risultato della potenza: %d", calcPower(base, exp));
+
+    return 0;
+}
